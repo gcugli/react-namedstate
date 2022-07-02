65ec8021cfa6d1d3594c8af25372045edfe1421a
gcugli <gcugli@gmail.com>
2022-07-01 23:37:00 -0300

    antd

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'antd';
+import { GithubOutlined } from '@ant-design/icons';
 
 function App() {
   return (
@@ -7,16 +9,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <Button icon={<GithubOutlined />}
+            href="https://github.com/gcugli/react-namedstate" target="_blank">
+            react-namedstate
+          </Button>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
